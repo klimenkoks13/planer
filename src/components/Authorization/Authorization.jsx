0fe9ba9e2b76b3ee468fe8fc2c769a64b989d4c5
klimkoks <ksenya_klim13@list.ru>
2026-05-30 18:38:18 +0300

    изменение авторизации

--- src/components/Authorization/Authorization.jsx
+++ src/components/Authorization/Authorization.jsx
@@ -1,30 +1,26 @@
 import AuthorizationLoginForm from "../AuthorizationLoginForm/AuthorizationLoginForm.jsx";
 import classes from './Authorization.module.css'
+import {features} from "../../data.js";
+import Features from "../Features.jsx";
 
 export default function Authorization() {
     return (
-        <section>
-            <div className={classes.authorization_branding}>
+        <section className={classes.authorizationPage}>
+            <div className={classes.authorizationBranding}>
                 <div className={classes.logo}>Forma</div>
                 <h1>Ваш день, <br/>
                     ваши <br/>
                     правила.</h1>
                 <p>Планируйте задачи, отслеживайте привычки и анализируйте свой прогресс в одном красивом пространстве.</p>
-                <div>
-                    <img/>
-                    <p>Умный дневной планировщик</p>
-                </div>
-                <div>
-                    <img/>
-                    <p>Трекер привычек и настроения</p>
-                </div>
-                <div>
-                    <img/>
-                    <p>Глубокая аналитика</p>
-                </div>
 
+                <div className={classes.features}>
+                    {features.map((feature) =>
+                        <Features feature={feature} key={feature.id} />
+                    )}
+                </div>
             </div>
-            <div  className={classes.authorization}>
+
+            <div  className={classes.authorizationForm}>
                 <AuthorizationLoginForm/>
             </div>
         </section>
